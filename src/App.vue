88385534d6d83ac15d0eeb03bb4ae71077763269
Robson de Moraes Silva <rms2307@outlook.com>
2023-09-18 10:29:30 -0300

    Implementa lista de livros

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <script setup>
-import { ref } from "vue"
-let count = ref(0)
+import Books from "./components/Books.vue"
+let books = [
+  {
+    id: 1,
+    title: "History of Europe",
+    cover:
+      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
+    isRead: true,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo",
+  },
+  {
+    id: 2,
+    title: "Penguin Classics",
+    cover:
+      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo, Jon Snow",
+  },
+  {
+    id: 3,
+    title: "Becoming",
+    cover:
+      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo",
+  },
+  {
+    id: 4,
+    title: "Sonnets",
+    cover:
+      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo",
+  },
+]
 </script>
 
 <template>
-  <div>
-    <h1>{{ count }}</h1>
-    <button @click="count++">Incrementar</button>
+  <div class="container">
+    <h1>📖 Meus Livros</h1>
+    <div class="header-btns">
+      <button class="btn">Adicionar Livro +</button>
+    </div>
+
+    <div class="books-container">
+      <Books :books="books" />
+    </div>
   </div>
 </template>
 
