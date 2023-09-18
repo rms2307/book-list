7aaff8030faad19020112fe19997c07e789480ea
Robson de Moraes Silva <rms2307@outlook.com>
2023-09-17 23:38:16 -0300

    Inicia projeto Vue + Vite + Composition API

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue"
+let count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Incrementar</button>
+  </div>
+</template>
+
+<style scoped></style>
